80efc053bcb57ffffbf70c2fc5927ced033597d8
Zoltan Altfatter <altfatterz@gmail.com>
2017-09-21 15:45:58 +0200

    initial commit

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -1,12 +1,14 @@
 package com.example;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBatchWithJmsApplication {
+@EnableBatchProcessing
+public class MyApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBatchWithJmsApplication.class, args);
+		SpringApplication.run(MyApp.class, args);
 	}
 }
