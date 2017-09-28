86a991c1066ad02a3f432e9f87e2c0c287f193e2
Zoltan Altfatter <altfatterz@gmail.com>
2017-09-28 10:53:50 +0200

    cleanup

--- inboud-channel-adapter-example/src/main/java/com/example/SendEmailStep.java
+++ inboud-channel-adapter-example/src/main/java/com/example/SendEmailStep.java
@@ -2,7 +2,6 @@ package com.example;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
